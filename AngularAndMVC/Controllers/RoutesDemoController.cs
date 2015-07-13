e0d80ee38c1d47fccb462b96e61ec25a63d4d8b9
Samuel Gladstone <gladston@umich.edu>
2015-07-13 17:14:35 -0400

    Add basic routing

---
+++ AngularAndMVC/Controllers/RoutesDemoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularAndMVC.Controllers
+{
+    public class RoutesDemoController : Controller
+    {
+        public ActionResult One()
+        {
+            return View();
+        }
+
+        public ActionResult Two()
+        {
+            return View();
+        }
+
+        public ActionResult Three()
+        {
+            return View();
+        }
+    }
+}
--- AngularAndMVC/Scripts/AngularAndMVC.js
+++ AngularAndMVC/Scripts/AngularAndMVC.js
@@ -1,3 +1,19 @@
-﻿var AngularAndMVC = angular.module('AngularAndMVC', []);
+﻿var AngularAndMVC = angular.module('AngularAndMVC', ['ngRoute']);
 
 AngularAndMVC.controller('LandingPageController', LandingPageController);
+
+var configFunction = function ($routeProvider) {
+    $routeProvider.
+        when('/routeOne', {
+            templateUrl: 'routesDemo/one'
+        })
+        .when('/routeTwo', {
+            templateUrl: 'routesDemo/two'
+        })
+        .when('/routeThree', {
+            templateUrl: 'routesDemo/three'
+        });
+}
+configFunction.$inject = ['$routeProvider'];
+
+AngularAndMVC.config(configFunction);
